40d4922d4d0e4d65b6e49cf79cbf8c431af5c1c4
Anwesh43 <anweshthecool0@gmail.com>
2019-10-12 12:38:19 +0530

    adding constants and libraies

--- linecircuitbreakerview/src/main/java/com/anwesh/uiprojects/linecircuitbreakerview/LineCircuitBreakerView.kt
+++ linecircuitbreakerview/src/main/java/com/anwesh/uiprojects/linecircuitbreakerview/LineCircuitBreakerView.kt
@@ -3,3 +3,18 @@ package com.anwesh.uiprojects.linecircuitbreakerview
 /**
  * Created by anweshmishra on 12/10/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+
+val nodes : Int = 5
+val scGap : Float = 0.01f
+val strokeFactor : Int = 90
+val delay : Long = 20
+val foreColor : Int = Color.parseColor("#9C27B0")
+val backColor : Int = Color.parseColor("#BDBDBD")

973f5939222735faf466f84256e5ad5607242f79
Anwesh43 <anweshthecool0@gmail.com>
2019-10-12 13:04:17 +0530

    adding LineCircuitBreak class

--- linecircuitbreakerview/src/main/java/com/anwesh/uiprojects/linecircuitbreakerview/LineCircuitBreakerView.kt
+++ linecircuitbreakerview/src/main/java/com/anwesh/uiprojects/linecircuitbreakerview/LineCircuitBreakerView.kt
@@ -151,6 +151,28 @@ class LineCircuitBreakerView(ctx : Context) : View(ctx) {
             cb()
             return this
         }
+    }
 
+    data class LineCircuitBreak(var i : Int) {
+
+        private var curr : LCBNode = LCBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
     }
 }
